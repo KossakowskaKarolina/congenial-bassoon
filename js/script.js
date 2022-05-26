5ff6db5e871d4504b0ff3329297cc4e12d875043
Karolina Kossakowska <kossakowska.karolina1@gmail.com>
2022-05-26 22:52:43 +0200

    Animate hamburger menu to hid on click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // Toggle class on hamburger menu
 
 const menu = document.querySelector('.navigation__hamburger');
+const navMenu = document.querySelector('.navigation__menu');
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('navigation__hamburger--active');
+  navMenu.classList.toggle('navigation__menu--hidden');
 });
